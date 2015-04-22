7418bf7b8c1952854b743ec27d6a38be0891e321
TechTalents <hello@techtalents.es>
2015-04-22 19:55:39 +0200

    HAY QUE DESLOGUEARSE

---
+++ The Smasher/src/main/Bloque.java
@@ -0,0 +1,20 @@
+package main;
+
+import es.techtalents.ttgdl.sprite.Sprite;
+
+public class Bloque extends Sprite{
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onColision(Sprite arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
