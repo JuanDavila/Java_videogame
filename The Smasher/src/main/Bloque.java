706418553dcd46c73498dd51a5abeac0be225d5d
TechTalents <hello@techtalents.es>
2015-04-29 17:37:32 +0200

    Los ladrillos cambian de color

--- The Smasher/src/main/Bloque.java
+++ The Smasher/src/main/Bloque.java
@@ -5,40 +5,52 @@ import java.awt.Image;
 import es.techtalents.ttgdl.geom.Vector2f;
 import es.techtalents.ttgdl.image.ImageLoader;
 import es.techtalents.ttgdl.sprite.Sprite;
- 
+
 public class Bloque extends Sprite{
-	Vector2f speed = new Vector2f(0,0);
-	private int vida = 1;
+	
+	Vector2f speed = new Vector2f(0,1);
+	private int vida = 3;
+	
+	Image img3 = ImageLoader.loadImage("Images/bloque_dorado.png");
+	Image img2 = ImageLoader.loadImage("Images/bloque_morada.png");
+	Image img = ImageLoader.loadImage("Images/bloque_azul.png");
 	
 	public Bloque(){
-		Image img = ImageLoader.loadImage("Images/bloque_azul.png");
-		setImage(img);
+
+		setImage(img3);
 	}
 
 	@Override
 	public void act() {
 		// TODO Auto-generated method stub
-		getPosition().add(speed); 
+		if (isDead()){
+			getPosition().add(speed); 
+		}
 	}
 
 	@Override
 	public void onColision(Sprite arg0) {
 		// TODO Auto-generated method stub
 		if (arg0 instanceof Pelota){
-			speed.y = 1;
 			vida--;
+
+			if (vida == 3){
+				setImage(img3);
+			}
+
+			if (vida == 2){
+				setImage(img2);
+			}
+			if (vida <= 1){
+				setImage(img);
+			}
 		}
+
 	}
 
 	public boolean isDead() {
-		if (vida <= 0){
-			return true;
-		}
-		else {
-			return false;
-		}
-		
+		return vida <= 0;
 	}
-	
+
 
 }
